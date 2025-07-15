ecd5277700b5f7af23ea0e6586a12826716ba04b
Lisa Nguyen <ngocanhthu.work@gmail.com>
2025-07-15 19:14:14 -0400

    Change navItems

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,8 +5,8 @@ const navItems = [
   { name: 'Home', href: '#home' },
   { name: 'About', href: '#about' },
   { name: 'Projects', href: '#projects' },
-  { name: 'Experience', href: '#experience' },
   { name: 'Skills', href: '#skills' },
+  { name: 'Experience', href: '#experience' },
   { name: 'Contact', href: '#contact' }
 ];
 
